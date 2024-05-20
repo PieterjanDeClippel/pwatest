096b001769699429414523d4f8e5c8186f5f0222
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-05-20 12:12:22 +0200

    Add button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'wopen';
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngAfterViewInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe({
+        next: upd => {
+          if (upd.type === 'VERSION_READY') {
+            this.swUpdate.activateUpdate()
+              .then(() => {
+                alert('Update was installed. Reloading...');
+                window.location.reload();
+              });
+          }
+        },
+        error: error => console.error(error)
+      });
+      this.swUpdate.checkForUpdate();
+    }
+  }
+
   openWindow() {
-    const w = window.open("", "_blank", "width=600,height=400");
+    const w = window.open(`${window.location.origin}/Account/ExternalLogin/Microsoft`, '_blank', 'width=600,height=400');
+  }
+
+  openWindowUsingAssign() {
+    const w = window.open('', '_blank', 'width=600,height=400');
     if (w) {
-      const host = window.location.origin;
-      w.document.location.assign(host + "/Account/ExternalLogin/Microsoft");
+      w.document.location.assign(`${window.location.origin}/Account/ExternalLogin/Microsoft`);
     }
   }
 }
