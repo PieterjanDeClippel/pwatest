92dde16f9501fa8cdbf3721ed35b45cedb709103
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-05-20 09:36:52 +0200

    Working demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'wopen';
+  openWindow() {
+    const w = window.open("", "_blank", "width=600,height=400");
+    if (w) {
+      const host = window.location.origin;
+      w.document.location.assign(host + "/Account/ExternalLogin/Microsoft");
+    }
+  }
 }
